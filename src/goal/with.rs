c7ac299e61326b19d6faf0972771c1ec083900f4
Erik Simmler <tgecho@gmail.com>
2020-01-20 23:30:29 -0500

    Got with goal working!

--- src/goal/with.rs
+++ src/goal/with.rs
@@ -2,22 +2,34 @@ use super::Goal;
 use crate::lvar::LVar;
 use crate::state::State;
 
-pub fn with<T: Eq + Clone + 'static>(
-    func: Box<dyn FnOnce(LVar) -> Box<dyn Goal<T>>>,
-) -> impl Goal<T> {
-    WithGoal {
-        func: Box::new(func),
+pub fn with<T: Eq + Clone + 'static, G: Goal<T>>(func: fn(LVar) -> G) -> impl Goal<T> {
+    WithGoal { func }
+}
+
+struct WithGoal<G> {
+    func: fn(LVar) -> G,
+}
+
+impl<T: Eq + Clone, G: Goal<T>> Goal<T> for WithGoal<G> {
+    fn run<'a>(self, state: &'a State<T>) -> Box<dyn Iterator<Item = State<T>> + 'a> {
+        let goal = self.func;
+        goal(LVar::new()).run(state)
     }
 }
 
-struct WithGoal<T: Eq + Clone> {
-    func: Box<dyn FnOnce(LVar) -> Box<dyn Goal<T>>>,
-}
-
-impl<T: Eq + Clone> Goal<T> for WithGoal<T> {
-    fn run<'a>(self, state: &'a State<T>) -> Box<dyn Iterator<Item = State<T>> + 'a> {
-        let func = self.func;
-        let goal = func(LVar::new());
-        goal.run(state)
+#[cfg(test)]
+mod tests {
+    use super::with;
+    use crate::goal::equal::equal;
+    use crate::goal::Goal;
+    use crate::state::{Cell, State};
+    #[test]
+    fn basic_equal() {
+        let state: State<u32> = State::new();
+        let goal = with(|x| equal(Cell::Var(x), Cell::Value(5)));
+        let mut result = goal.run(&state);
+        // TODO: Use one of the fancier (as of yet unimplemented) goals to
+        // inject a variable we can use to check this result
+        assert!(result.nth(0).is_some());
     }
 }
