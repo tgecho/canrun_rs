7ca213fb63434bd3d4134700948cfdeb50e42217
Erik Simmler <tgecho@gmail.com>
2020-01-25 18:33:47 -0500

    Add a not not test case

--- src/goal/not.rs
+++ src/goal/not.rs
@@ -16,7 +16,7 @@ mod tests {
         assert_eq!(results.nth(0), None);
     }
     #[test]
-    fn nested_not() {
+    fn not_combined() {
         let state: State<u32> = State::new();
         let x = LVar::new();
         let goal = any(vec![
@@ -33,5 +33,18 @@ mod tests {
         let goal = both(goal, not(equal(x.into(), Cell::Value(1))));
         let results: Vec<_> = goal.clone().run(&state).map(|s| s.resolve_var(x)).collect();
         assert_eq!(results, vec![Cell::Value(3), Cell::Value(2)]);
+    }
+
+    #[test]
+    fn not_not() {
+        let state: State<u32> = State::new();
+        let x = LVar::new();
+        let goal = not(not(equal(x.into(), Cell::Value(1))));
+        let results: Vec<_> = goal.clone().run(&state).map(|s| s.resolve_var(x)).collect();
+        // I'm not actually sure if this result makes sense or is what we want
+        assert_eq!(results, vec![x.into()]);
+
+        let goal = not(not(equal(Cell::Value(1), Cell::Value(1))));
+        assert!(goal.clone().run(&state).nth(0).is_some());
     }
 }
