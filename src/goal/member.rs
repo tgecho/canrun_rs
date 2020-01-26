a8c6416c3fe28a7f6cfcbf8f925ea28417742156
Erik Simmler <tgecho@gmail.com>
2020-01-26 16:14:44 -0500

    Proved using with more complex data shapes

--- src/goal/member.rs
+++ src/goal/member.rs
@@ -15,7 +15,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::member;
-    use crate::{both, equal, Cell, LVar, State};
+    use crate::{any, both, equal, Cell, LVar, Pair, State};
     #[test]
     fn basic_member() {
         let x = LVar::new();
@@ -38,5 +38,79 @@ mod tests {
         );
         let result: Vec<_> = goal.run(State::new()).map(|r| r.resolve_var(x)).collect();
         assert_eq!(result, vec![Cell::Value(2)]);
+    }
+    #[test]
+    fn member_with_pairs() {
+        let x = LVar::new();
+        let y = LVar::new();
+
+        fn rel<T: Eq + Clone>(a: Cell<T>, b: Cell<T>, c: Cell<T>) -> Cell<T> {
+            Pair::new(a, Pair::new(b, c))
+        }
+
+        let find = |desired| {
+            let goal = both(
+                equal(x.into(), desired),
+                member(
+                    x.into(),
+                    vec![
+                        rel(Cell::Value(0), Cell::Value(1), Cell::Value(2)),
+                        rel(Cell::Value(3), Cell::Value(4), Cell::Value(5)),
+                    ]
+                    .into_iter(),
+                ),
+            );
+            let result: Vec<_> = goal
+                .run(State::new())
+                .map(|r| (r.resolve_var(y), r.resolve_var(x)))
+                .collect();
+            result
+        };
+
+        assert_eq!(
+            find(rel(y.into(), Cell::Value(1), Cell::Value(2))),
+            vec![(
+                Cell::Value(0),
+                rel(Cell::Value(0), Cell::Value(1), Cell::Value(2))
+            )]
+        );
+
+        assert_eq!(
+            find(rel(Cell::Value(0), y.into(), Cell::Value(2))),
+            vec![(
+                Cell::Value(1),
+                rel(Cell::Value(0), Cell::Value(1), Cell::Value(2))
+            )]
+        );
+
+        assert_eq!(find(rel(Cell::Value(1), y.into(), Cell::Value(2))), vec![]);
+    }
+    #[test]
+    fn member_with_pairs_complex() {
+        let x = LVar::new();
+        let y = LVar::new();
+
+        fn rel<T: Eq + Clone>(a: Cell<T>, b: Cell<T>, c: Cell<T>) -> Cell<T> {
+            Pair::new(a, Pair::new(b, c))
+        }
+
+        let goal = both(
+            any(vec![
+                equal(x.into(), rel(Cell::Value(0), y.into(), Cell::Value(2))),
+                equal(x.into(), rel(y.into(), Cell::Value(1), Cell::Value(2))),
+                equal(x.into(), rel(Cell::Value(3), Cell::Value(4), y.into())),
+            ]),
+            member(
+                x.into(),
+                vec![
+                    rel(Cell::Value(0), Cell::Value(1), Cell::Value(2)),
+                    rel(Cell::Value(3), Cell::Value(4), Cell::Value(5)),
+                ]
+                .into_iter(),
+            ),
+        );
+        let result: Vec<_> = goal.run(State::new()).map(|r| r.resolve_var(y)).collect();
+
+        assert_eq!(result, vec![Cell::Value(5), Cell::Value(0), Cell::Value(1)]);
     }
 }
