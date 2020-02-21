1b7b5ee9948272c3ab2d0913141f6fc600e9f131
Erik Simmler <tgecho@gmail.com>
2020-02-20 22:10:30 -0500

    Roughed out parameterized tests to show that the first pass at constrain is flawed

--- src/goal/constrain.rs
+++ src/goal/constrain.rs
@@ -43,3 +43,98 @@ pub fn constrain<'a, T: CanT>(a: Can<T>, b: Can<T>, func: fn(T, T) -> bool) -> G
         func,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{all, Can, CanT, Goal, LVar, State};
+    use galvanic_test::test_suite;
+    use itertools::Itertools;
+    use std::iter::once;
+
+    fn all_permutations<'a, T: CanT + 'a>(
+        goals: Vec<Goal<'a, T>>,
+        vars: Vec<LVar>,
+    ) -> impl Iterator<Item = (Vec<Goal<'a, T>>, Vec<LVar>)> + 'a {
+        let goals_len = goals.len();
+        goals
+            .into_iter()
+            .permutations(goals_len)
+            .zip(once(vars).cycle())
+    }
+
+    fn resolve_to<'a, T: CanT + 'a>(
+        goals: &Vec<Goal<'a, T>>,
+        vars: &Vec<LVar>,
+    ) -> Vec<Vec<Can<T>>> {
+        all(goals.clone())
+            .run(State::new())
+            .map(|s| {
+                let results = vars.iter().map(|v| s.resolve_var(*v).unwrap());
+                results.collect::<Vec<Can<T>>>()
+            })
+            .collect()
+    }
+
+    test_suite! {
+        use crate::{constrain, Can, Equals, var,Goal, LVar};
+        use super::{all_permutations, resolve_to};
+
+        fixture success_cases(goals: Vec<Goal<'static, usize>>, vars: Vec<LVar>) -> Vec<Vec<Can<usize>>> {
+            params {
+                let (x, y) = (var(), var());
+                let goals = vec![
+                    constrain(x.can(), y.can(), |x, y| x < y),
+                    x.equals(1),
+                    y.equals(2),
+                ];
+                all_permutations(goals, vec![x, y])
+            }
+            setup(&mut self) {
+                resolve_to(self.goals, self.vars)
+            }
+        }
+
+        test success(success_cases) {
+            assert_eq!(success_cases.val, vec![vec![Can::Val(1), Can::Val(2)]])
+        }
+
+        fixture fail_cases(goals: Vec<Goal<'static, usize>>, vars: Vec<LVar>) -> Vec<Vec<Can<usize>>> {
+            params {
+                let (x, y) = (var(), var());
+                let goals = vec![
+                    constrain(x.can(), y.can(), |x, y| x > y),
+                    x.equals(1),
+                    y.equals(2),
+                ];
+                all_permutations(goals, vec![x, y])
+            }
+            setup(&mut self) {
+                resolve_to(self.goals, self.vars)
+            }
+        }
+
+        test fail(fail_cases) {
+            assert!(fail_cases.val.is_empty())
+        }
+
+        fixture multiple_constraints_cases(goals: Vec<Goal<'static, usize>>, vars: Vec<LVar>) -> Vec<Vec<Can<usize>>> {
+            params {
+                let (x, y) = (var(), var());
+                let goals = vec![
+                    constrain(x.can(), y.can(), |x, y| x < y),
+                    constrain(x.can(), y.can(), |x, y| x > y),
+                    x.equals(1),
+                    y.equals(2),
+                ];
+                all_permutations(goals, vec![x, y])
+            }
+            setup(&mut self) {
+                resolve_to(self.goals, self.vars)
+            }
+        }
+
+        test multiple_constraints(multiple_constraints_cases) {
+            assert!(multiple_constraints_cases.val.is_empty())
+        }
+    }
+}
