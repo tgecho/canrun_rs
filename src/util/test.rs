8d3632b95aa0c25dbcb5d6d8aebc0fb94e2e0ae9
Erik Simmler <tgecho@gmail.com>
2020-02-23 22:23:17 -0500

    Actually use a split out test helper function

--- src/util/test.rs
+++ src/util/test.rs
@@ -12,13 +12,7 @@ pub(crate) fn goals_resolve_to<'a, T: CanT + 'a>(
     goals: &Vec<Goal<'a, T>>,
     vars: &Vec<LVar>,
 ) -> Vec<Vec<Can<T>>> {
-    all(goals.clone())
-        .run(State::new())
-        .map(|s| {
-            let results = vars.iter().map(|v| s.resolve_var(*v).unwrap());
-            results.collect::<Vec<Can<T>>>()
-        })
-        .collect()
+    states_resolve_to(all(goals.clone()).run(State::new()), vars)
 }
 
 pub(crate) fn states_resolve_to<'a, T: CanT + 'a>(
