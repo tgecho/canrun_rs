d38fe4b9cafb7f3d1922f3c9a0f664a0a369804a
Erik Simmler <tgecho@gmail.com>
2020-03-08 23:41:53 -0400

    Move the rest of the unported old code into a folder for containment/reference

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
-// pub mod can;
-// pub mod constraint;
-// pub mod goal;
-// pub mod state;
-// pub(crate) mod util;
-
 pub mod core;
-
-// pub use can::lvar::{var, LVar};
-// pub use can::pair::pair;
-// pub use can::{val, Can, CanT};
-// pub use state::{ResolveResult, State, UnifyError, UnifyResult};
-
-// // Goals
-// pub use goal::both::both;
-// pub use goal::either::either;
-// pub use goal::equal::{equal, Equals};
-// pub use goal::lazy::{lazy, with1, with2, with3};
-// pub use goal::not::not;
-// pub use goal::Goal;
-// pub use goal::StateIter;
-
-// pub use goal::extra::all::all;
-// pub use goal::extra::any::any;
-// pub use goal::extra::append::append;
-// pub use goal::extra::constrain::{constrain_1, constrain_2};
-// pub use goal::extra::map::{map_2, map_3};
-// pub use goal::extra::member::member;
 
 #[macro_use]
 extern crate log;
