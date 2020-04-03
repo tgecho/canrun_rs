803549261b3283ad9c8138c702103ba166beadac
Erik Simmler <tgecho@gmail.com>
2020-04-02 22:44:03 -0400

    Add a tuple3 Unify impl

--- core/src/unify/tuples.rs
+++ core/src/unify/tuples.rs
@@ -17,36 +17,100 @@ where
     }
 }
 
+impl<'a, A, B, C, D> Unify<'a, (Val<A>, Val<B>, Val<C>)> for State<'a, D>
+where
+    D: DomainType<'a, A>
+        + DomainType<'a, B>
+        + DomainType<'a, C>
+        + DomainType<'a, (Val<A>, Val<B>, Val<C>)>,
+    Self: Unify<'a, A> + Unify<'a, B> + Unify<'a, C>,
+{
+    fn unify_resolved(
+        self,
+        l: Rc<(Val<A>, Val<B>, Val<C>)>,
+        r: Rc<(Val<A>, Val<B>, Val<C>)>,
+    ) -> Option<Self> {
+        Some(
+            self.unify(l.0.clone(), r.0.clone())?
+                .unify(l.1.clone(), r.1.clone())?
+                .unify(l.2.clone(), r.2.clone())?,
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate as canrun;
     use crate::goal::unify;
     use crate::goal::Goal;
-    use crate::tests::domains::Numbers2;
     use crate::util;
-    use crate::value::{val, var};
+    use crate::value::{val, var, Val};
+    use canrun_codegen::domains;
+
+    domains! {
+        pub domain Tuples2 {
+            i32,
+            (Val<i32>, Val<i32>),
+        }
+        pub domain Tuples3 {
+            i32,
+            (Val<i32>, Val<i32>, Val<i32>),
+        }
+    }
 
     #[test]
-    fn succeeds() {
+    fn tuple2_succeeds() {
         let x = var();
-        let goals: Vec<Goal<Numbers2>> =
+        let goals: Vec<Goal<Tuples2>> =
             vec![unify(x, (val(1), val(2))), unify(x, (val(1), val(2)))];
         util::all_permutations_resolve_to(goals, x, vec![(val(1), val(2))]);
     }
 
     #[test]
-    fn fails() {
+    fn tuple2_fails() {
         let x = var();
-        let goals: Vec<Goal<Numbers2>> =
+        let goals: Vec<Goal<Tuples2>> =
             vec![unify(x, (val(1), val(3))), unify(x, (val(1), val(2)))];
         util::all_permutations_resolve_to(goals, x, vec![]);
     }
 
     #[test]
-    fn nested_var() {
+    fn tuple2_nested_var() {
         let x = var();
         let y = var();
-        let goals: Vec<Goal<Numbers2>> =
+        let goals: Vec<Goal<Tuples2>> =
             vec![unify(x, (val(1), y.into_val())), unify(x, (val(1), val(2)))];
+        util::all_permutations_resolve_to(goals, y, vec![2]);
+    }
+
+    #[test]
+    fn tuple3_succeeds() {
+        let x = var();
+        let goals: Vec<Goal<Tuples3>> = vec![
+            unify(x, (val(1), val(2), val(3))),
+            unify(x, (val(1), val(2), val(3))),
+        ];
+        util::all_permutations_resolve_to(goals, x, vec![(val(1), val(2), val(3))]);
+    }
+
+    #[test]
+    fn tuple3_fails() {
+        let x = var();
+        let goals: Vec<Goal<Tuples3>> = vec![
+            unify(x, (val(1), val(2), val(3))),
+            unify(x, (val(1), val(2), val(4))),
+        ];
+        util::all_permutations_resolve_to(goals, x, vec![]);
+    }
+
+    #[test]
+    fn tuple3_nested_var() {
+        let x = var();
+        let y = var();
+        let goals: Vec<Goal<Tuples3>> = vec![
+            unify(x, (val(1), y.into_val(), val(3))),
+            unify(x, (val(1), val(2), val(3))),
+        ];
         util::all_permutations_resolve_to(goals, y, vec![2]);
     }
 }
