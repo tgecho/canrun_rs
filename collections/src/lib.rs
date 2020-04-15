de6bf265e90f4bddba98af28f10a41b045a8fd47
Erik Simmler <tgecho@gmail.com>
2020-04-15 19:12:59 -0400

    Add comparison benchmark

--- collections/src/lib.rs
+++ collections/src/lib.rs
@@ -1,6 +1,13 @@
 #![warn(missing_docs)]
 
-//! [Canrun](canrun) collection related [goal](canrun) functions and other miscellany.
+//! [Canrun](canrun) collections and related [goal](canrun) functions.
+//!
+//! # NOTE: These are not very battle tested and may have some pathological performance characteristics.
+//!
+//! Unifying large or complex collections may involve forking the state for
+//! every possible combination of values. Also, the inherent complexity of
+//! specifying and implementing these operations correctly means that they could
+//! be flat out wrong. More testing, benchmarking and refinement is required.
 
 extern crate canrun;
 
