ea7d72f29416c11f5b666d41f2447a1ef049b386
Erik Simmler <tgecho@gmail.com>
2020-07-01 20:22:52 -0400

    Streamline zebra helpers by using more refs

--- examples/src/zebra.rs
+++ examples/src/zebra.rs
@@ -28,29 +28,17 @@ fn v() -> Val<&'static str> {
     Val::Var(var())
 }
 
-fn on_right<'a>(left: LHouse, right: LHouse, houses: LVec<LHouse>) -> Goal<'a, Zebra> {
+fn on_right<'a>(left: &LHouse, right: &LHouse, houses: &LVec<LHouse>) -> Goal<'a, Zebra> {
     any![
-        unify(
-            lvec![left.clone(), right.clone(), var(), var(), var()],
-            houses.clone()
-        ),
-        unify(
-            lvec![var(), left.clone(), right.clone(), var(), var()],
-            houses.clone()
-        ),
-        unify(
-            lvec![var(), var(), left.clone(), right.clone(), var()],
-            houses.clone()
-        ),
+        unify(lvec![left, right, var(), var(), var()], houses),
+        unify(lvec![var(), left, right, var(), var()], houses),
+        unify(lvec![var(), var(), left, right, var()], houses),
         unify(lvec![var(), var(), var(), left, right], houses),
     ]
 }
 
-fn next_to(a: LHouse, b: LHouse, houses: LVec<LHouse>) -> Goal<'static, Zebra> {
-    either(
-        on_right(a.clone(), b.clone(), houses.clone()),
-        on_right(b, a, houses),
-    )
+fn next_to(a: &LHouse, b: &LHouse, houses: &LVec<LHouse>) -> Goal<'static, Zebra> {
+    either(on_right(a, b, houses), on_right(b, a, houses))
 }
 
 pub fn zebra() -> Vec<Vec<House>> {
@@ -62,60 +50,39 @@ pub fn zebra() -> Vec<Vec<House>> {
         var(),
     ];
     let goal: Goal<Zebra> = all![
-        member(
-            (val!("englishman"), v(), v(), v(), val!("red")),
-            houses.clone()
-        ),
+        member((val!("englishman"), v(), v(), v(), val!("red")), &houses),
         on_right(
-            (v(), v(), v(), v(), val!("ivory")),
-            (v(), v(), v(), v(), val!("green")),
-            houses.clone()
+            &(v(), v(), v(), v(), val!("ivory")),
+            &(v(), v(), v(), v(), val!("green")),
+            &houses
         ),
         next_to(
-            (val!("norwegian"), v(), v(), v(), v()),
-            (v(), v(), v(), v(), val!("blue")),
-            houses.clone()
+            &(val!("norwegian"), v(), v(), v(), v()),
+            &(v(), v(), v(), v(), val!("blue")),
+            &houses
         ),
-        member(
-            (v(), val!("kools"), v(), v(), val!("yellow")),
-            houses.clone()
-        ),
-        member(
-            (val!("spaniard"), v(), v(), val!("dog"), v()),
-            houses.clone()
-        ),
-        member(
-            (v(), v(), val!("coffee"), v(), val!("green")),
-            houses.clone()
-        ),
-        member(
-            (val!("ukrainian"), v(), val!("tea"), v(), v()),
-            houses.clone()
-        ),
-        member(
-            (v(), val!("luckystrikes"), val!("oj"), v(), v()),
-            houses.clone()
-        ),
+        member((v(), val!("kools"), v(), v(), val!("yellow")), &houses),
+        member((val!("spaniard"), v(), v(), val!("dog"), v()), &houses),
+        member((v(), v(), val!("coffee"), v(), val!("green")), &houses),
+        member((val!("ukrainian"), v(), val!("tea"), v(), v()), &houses),
+        member((v(), val!("luckystrikes"), val!("oj"), v(), v()), &houses),
         member(
             (val!("japanese"), val!("parliaments"), v(), v(), v()),
-            houses.clone()
+            &houses
         ),
-        member(
-            (v(), val!("oldgolds"), v(), val!("snails"), v()),
-            houses.clone()
+        member((v(), val!("oldgolds"), v(), val!("snails"), v()), &houses),
+        next_to(
+            &(v(), v(), v(), val!("horse"), v()),
+            &(v(), val!("kools"), v(), v(), v()),
+            &houses
         ),
         next_to(
-            (v(), v(), v(), val!("horse"), v()),
-            (v(), val!("kools"), v(), v(), v()),
-            houses.clone()
+            &(v(), v(), v(), val!("fox"), v()),
+            &(v(), val!("chesterfields"), v(), v(), v()),
+            &houses
         ),
-        next_to(
-            (v(), v(), v(), val!("fox"), v()),
-            (v(), val!("chesterfields"), v(), v(), v()),
-            houses.clone()
-        ),
-        member((v(), v(), val!("water"), v(), v()), houses.clone()),
-        member((v(), v(), v(), val!("zebra"), v()), houses.clone()),
+        member((v(), v(), val!("water"), v(), v()), &houses),
+        member((v(), v(), v(), val!("zebra"), v()), &houses),
     ];
     goal.query(houses).collect()
 }
