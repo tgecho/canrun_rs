5be03e5e4a5d39c1d8562f880448c9bda05c98bc
Erik Simmler <tgecho@gmail.com>
2021-04-21 21:12:38 -0400

    Ditch the readme check in favor of just including the readme into the root lib.rs
    This depends on nightly because of extended_key_value_attributes (https://github.com/rust-lang/rust/pull/83366)
    But this appears to be close to stabilizing, and will likely be stable by the time I get around to cutting a release.

--- canrun/src/lib.rs
+++ canrun/src/lib.rs
@@ -1,27 +1,6 @@
 #![warn(missing_docs)]
-//! Canrun is a [logic
-//! programming](https://en.wikipedia.org/wiki/Logic_programming) library
-//! inspired by the [\*Kanren](http://minikanren.org/) family of language DSLs.
-//!
-//! ## Status: Exploratory and Highly Experimental
-//!
-//! I'm still quite new to both Rust and logic programming, so there are likely
-//! to be rough edges. At best the goal is to be a useful implementation of the
-//! core concepts of a Kanren in way that is idiomatic to Rust. At worst it may
-//! just be a poor misinterpretation with fatal flaws.
-//!
-//! ## Quick Start
-//!
-//! ```rust
-//! use canrun::{Goal, both, unify, var};
-//! use canrun::example::I32;
-//!
-//! let x = var();
-//! let y = var();
-//! let goal: Goal<I32> = both(unify(x, y), unify(1, x));
-//! let result: Vec<_> = goal.query(y).collect();
-//! assert_eq!(result, vec![1])
-//! ```
+#![feature(extended_key_value_attributes)]
+#![doc = include_str!("../../README.md")]
 
 pub mod collections;
 pub mod domains;
