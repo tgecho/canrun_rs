36dba8cff3c15bf3fa932cbc860702c8b08c57ff
Erik Simmler <tgecho@gmail.com>
2022-12-31 18:04:52 -0500

    Copy value docs

--- canrun2/src/core/value.rs
+++ canrun2/src/core/value.rs
@@ -5,6 +5,24 @@ use std::{any::Any, fmt::Debug, rc::Rc};
 
 pub(crate) type VarId = usize;
 
+/** A logical variable that represents a potential value of type `T`.
+
+`LVars` are are passed into [goals](crate::goals) to relate
+[values](crate::Value) and other variables to each other. They can also be
+used to [query](crate::Query) for values in a
+[`ResolvedState`](crate::state::ResolvedState).
+
+The main reason to deal with an `LVar` directly as opposed to just
+[`Value::Var`](crate::core::Value::Var) is that `LVar` implements [`Copy`],
+which makes it more convenient when the same var is shared between various goals,
+closures and other places.
+
+The identity of each `LVar` is tracked using an internal id. While this id
+is visible through the `Debug` implementation, it should only be used for
+debugging purposes as no guarantees are made about the type or generation of
+the id value. Also, these ids are only valid within the context of a single
+execution. They cannot be safely persisted or shared between processes.
+*/
 #[derive(Debug, Copy)]
 pub struct LVar<T> {
     pub(crate) id: VarId,
@@ -22,6 +40,13 @@ fn get_id() -> VarId {
     COUNTER.fetch_add(1, Ordering::Relaxed)
 }
 impl<T: Unify> LVar<T> {
+    /// Create a new [logical var](LVar).
+    ///
+    /// # Example:
+    /// ```
+    /// use canrun2::LVar;
+    /// let x: LVar<isize> = LVar::new();
+    /// ```
     pub fn new() -> Self {
         LVar {
             id: get_id(),
@@ -36,16 +61,51 @@ impl<T: Unify> Default for LVar<T> {
     }
 }
 
+/**
+Contain individual resolved values or variables that can be bound through
+[unification](crate::Unify).
+*/
 #[derive(Debug, PartialEq)]
 pub enum Value<T: Unify> {
+    /**
+    A [logical variable](LVar).
+
+    Even though they do not contain a value, they are parameterized with the type
+    they can potentially be bound to. This ensures that they can only be unified
+    with values of the same type.
+    */
     Var(LVar<T>),
+    /** A resolved value.
+
+    When a state is split into an arbitrary number of [resolved
+    states](crate::state::ResolvedState), some of the internal data
+    structures often need to be cloned. In an attempt to avoid unnecessary
+    cloning of every value in the state, we wrap it in an [Rc] so that
+    references can be shared.
+    */
     Resolved(Rc<T>),
 }
 
 impl<T: Unify> Value<T> {
+    /// Create a new [logical `Value`](Value) wrapping a `T`.
+    ///
+    /// # Example:
+    /// ```
+    /// use canrun2::Value;
+    /// let x: Value<i32> = Value::new(1);
+    /// ```
     pub fn new(t: T) -> Value<T> {
         Value::Resolved(Rc::new(t))
     }
+    /// Create a new [logical `Value`](Value) with an unresolved [`Var`](Value::Var).
+    ///
+    /// This is a shorthand for `Value::Var(LVar::new())`.
+    ///
+    /// # Example:
+    /// ```
+    /// use canrun2::Value;
+    /// let x: Value<i32> = Value::var();
+    /// ```
     pub fn var() -> Value<T> {
         Value::Var(LVar::new())
     }
@@ -57,6 +117,16 @@ impl<T: Unify> Value<T> {
         }
     }
 
+    /** Return `T` if the `Value` is a resolved.
+
+    Example:
+    ```
+    use canrun2::Value;
+
+    let x: Value<i32> = Value::new(1);
+    assert_eq!(x.resolved(), Some(&1));
+    ```
+    */
     pub fn resolved(&self) -> Option<&T> {
         match self {
             Value::Var(_) => None,
